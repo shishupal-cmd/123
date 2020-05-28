857b4fdf26a1c55846b224345a082316e70f1a09
shishupal-cmd <64471690+shishupal-cmd@users.noreply.github.com>
2020-05-28 17:22:25 +0530

    change

--- mnist.py
+++ mnist.py
@@ -17,3 +17,4 @@ model.add(Dense(units=256 , activation='relu'))
 model.add(Dense(units=10 , activation = 'softmax'))
 from keras.optimizers import RMSprop
 model.compile(optimizer =RMSprop() , loss='categorical_crossentropy',metrics=['accuracy'])
+model.fit(X_train ,Y_train_cat ,epochs=100)
