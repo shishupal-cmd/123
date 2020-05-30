04a0d0b3949fcbeb26adb5212c35d7bf6d49ec3f
shishupal-cmd <64471690+shishupal-cmd@users.noreply.github.com>
2020-05-30 10:41:27 +0530

    tweak

--- tweak.py
+++ tweak.py
@@ -1,5 +1,2 @@
-activators:
-    - "model.add(Dense(units=256, activation='relu'))
-    - "model.add(Dense(units=200, activation='relu'))
-    - "model.add(Dense(units=150, activation='relu'))
-    - "model.add(Dense(units=100, activation='relu'))
+variables:
+    epochs=1
